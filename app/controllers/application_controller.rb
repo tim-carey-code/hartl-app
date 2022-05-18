409a85ada323546254e9b68424bae4c51305ca99
Tim <timcarey1989@gmail.com>
2022-05-18 14:43:43 -0400

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
 
-  def hello 
-    render html: "HELLO, WORLD!"
-  end
   
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup 
     @base_title = "Ruby on Rails Tutorial Sample App"
   end 
+  
+  test "should get root" do 
+    get root_url 
+    assert_response :success 
+  end
 
   test "should get home" do
     get static_pages_home_url
@@ -29,5 +34,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success 
     assert_select "title", "Contact | #{@base_title}" 
   end
+  
 
 end

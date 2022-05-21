05867f6faa8b93f34293cb71c212f60b9b03229b
Tim <timcarey1989@gmail.com>
2022-05-20 23:57:54 -0400

    working on updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,19 @@ class UsersController < ApplicationController
     end 
   end
 
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # handle successful update 
+    else
+      render "edit", status: :unprocessable_entity 
+    end 
+  end 
+  
   private 
 
     def user_params 
